7917de1015d4f442a35dd2de90b93e6081495a02
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2024-07-03 21:25:08 +0900

    n04

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,19 @@
         <div class="row">
             @foreach($allProducts as $product)
 
-                <div class="card">
-                    <img class="card-img-top" src="{{ asset('images/no_image.jpg') }}" alt="Card image cap">
-                    <div class="card-body">
-                        <h4 class="card-title">aaa</h4>
-                        <p class="card-text"></p>
-                    </div>
-                    <div class="card-body">
-                        <a href="" class="card-link">Add to Cart</a>
+                <div class="col-4">
+                    <div class="card">
+                        <img class="card-img-top" src="{{ asset('images/no_image.jpg') }}" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">aaa</h4>
+                            <p class="card-text"></p>
+                        </div>
+                        <div class="card-body">
+                            <a href="" class="card-link">Add to Cart</a>
+                        </div>
                     </div>
                 </div>
+                
 
             @endforeach   
         </div>
